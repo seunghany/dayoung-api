579598439756461c53fc05c79e322d94109506f5
Seunghan <seunghany2@gmail.com>
2020-11-05 13:02:21 +0900

    Fixed style Errors

--- com_dayoung_api/resources/actor.py
+++ com_dayoung_api/resources/actor.py
@@ -1,39 +1,33 @@
-from typing import List
 from flask import request
 from flask_restful import Resource, reqparse
 import json
-from flask import jsonify
 from com_dayoung_api.ext.db import db, openSession
 import pandas as pd
-import json
-import os
-from com_dayoung_api.utils.file_helper import FileReader
-import pandas as pd
-import numpy as np
 from sqlalchemy import func
-from pathlib import Path
-
 from com_dayoung_api.resources.crawling import Crawling
 
+
 class ActorPreprocess(object):
-    
     def __init__(self):
-        self.c = Crawling() # 이병헌 is given as default
-        
+        self.c = Crawling()  # 이병헌 is given as default
+
         # print(self.dataFrame)
-        
+
     def hook(self):
         dataFrame = self.c.crawl()
         # print(dataFrame)
         return dataFrame
 
+
 class ActorDto(db.Model):
     __tablename__ = 'actors'
-    __table_args__={'mysql_collate':'utf8_general_ci'}
-    # 'photoUrl', 'age', 'actor_id', 'name', 'realName', 'religion', 'agency', 'spouse', 'children', 'debutYear', 'gender'
+    __table_args__ = {'mysql_collate': 'utf8_general_ci'}
+    # 'photoUrl', 'age', 'actor_id', 'name', 'realName', 'religion', 'agency',
+    #  'spouse', 'children', 'debutYear', 'gender', 'state'
 
-    # columns=['photoUrl', 'age','name','realName','religion','agency', 'spouse', 'children','debutYear','actor_id']
-    actor_id: str = db.Column(db.String(30), primary_key = True, index = True)
+    # columns=['photoUrl', 'age','name','realName','religion','agency',
+    #          'spouse', 'children','debutYear','actor_id']
+    actor_id: str = db.Column(db.String(30), primary_key=True, index=True)
     name: str = db.Column(db.String(30))
     gender: str = db.Column(db.String(1))
     age: str = db.Column(db.String(30))
@@ -46,7 +40,8 @@ class ActorDto(db.Model):
     state: str = db.Column(db.String(1))
     photo_url: str = db.Column(db.String(200))
 
-    def __init__(self, photo_url, actor_id, name, gender, age, real_name, spouse, children, debut_year, agency, religion, state):
+    def __init__(self, photo_url, actor_id, name, gender, age, real_name,
+                 spouse, children, debut_year, agency, religion, state):
         self.photo_url = photo_url
         self.actor_id = actor_id
         self.name = name
@@ -62,19 +57,20 @@ class ActorDto(db.Model):
 
     def json(self):
         return {
-            'photo_url' : self.photo_url,
-            'actor_id' : self.actor_id,
-            'name' : self.name,
-            'gender' : self.gender,
-            'age' : self.age,
-            'real_name' : self.real_name,
-            'spouse' : self.spouse,
-            'children' : self.children,
-            'debut_year' : self.debut_year,
-            'religion' : self.religion,
-            'agency' : self.agency,
-            'state' : self.state
+            'photo_url': self.photo_url,
+            'actor_id': self.actor_id,
+            'name': self.name,
+            'gender': self.gender,
+            'age': self.age,
+            'real_name': self.real_name,
+            'spouse': self.spouse,
+            'children': self.children,
+            'debut_year': self.debut_year,
+            'religion': self.religion,
+            'agency': self.agency,
+            'state': self.state
         }
+
 
 class ActorVo:
     actor_id: str = ''
@@ -90,56 +86,26 @@ class ActorVo:
     debut_year: int = 0
     state: str = '0'
 
+
 Session = openSession()
 session = Session()
 actor_preprocess = ActorPreprocess()
-    
-    
-"""   
-# ==============================================================
-# ==============================================================
-# ====================     Preprocess  ============================
-# ==============================================================
-# ==============================================================
-"""
-
-
-
-# ==============================================================
-# ==============================================================
-# =================     Controller  ============================
-# ==============================================================
-# ==============================================================
-
-parser = reqparse.RequestParser()  # only allow price changes, no name changes allowed
-parser.add_argument('actor_id', type=str, required=True,
-                                        help='This field should be a actor_id')
 
 
 class ActorDao(ActorDto):
-    
-    @staticmethod   
+    @staticmethod
     def add(actor_name):
         crawl = Crawling(actor_name)
         df = crawl.crawl()
         actor = df.to_dict(orient="records")
         actor = actor[0]
         actor = ActorDto(**actor)
-        
-        # print("_________________________________________________________________")
-
-        # print("actor 타입 ", type(actor))
-        # print(actor['age'])
-        # print("여기 옴222222222222222222222?")
-        # actor = ActorDto(actor)
-        # print("actor 타입 ", type(actor))
         Session = openSession()
         session = Session()
-        print("--------------------------------------------------------------------------")
+        print("---------------------------------------------")
         db.session.add(actor)
         db.session.commit()
         session.close()
-
 
     def bulk():
         df = actor_preprocess.hook()
@@ -163,7 +129,7 @@ class ActorDao(ActorDto):
         db.session.commit()
 
     @classmethod
-    def delete(cls,id):
+    def delete(cls, id):
         # this deletes actor from the whole database
         data = cls.query.get(id)
         db.session.delete(data)
@@ -175,12 +141,10 @@ class ActorDao(ActorDto):
         sql = cls.query
         df = pd.read_sql(sql.statement, sql.session.bind)
         return json.loads(df.to_json(orient='records'))
-    
+
     @classmethod
     def find_state_one(cls):
         return session.query(ActorDto).filter(ActorDto.state.like("1")).all()
-        
-        # session.query(ActorDto).filter(ActorDto.state.like("1")).all()
 
     @classmethod
     def find_by_name(cls, name):
@@ -191,12 +155,11 @@ class ActorDao(ActorDto):
         return session.query(ActorDto).filter(ActorDto.actor_id.like(f'{actor_id}')).one()
 
     @classmethod
-    def find_id_by_name(cls,name):
+    def find_id_by_name(cls, name):
         # 여기 나중에 조금 고쳐야 할 함
         actor = session.query(ActorDto).filter(ActorDto.name.like(f'{name}')).one()
         print(actor.actor_id)
         return actor.actor_id
-
 
     @classmethod
     def login(cls, actor):
@@ -208,27 +171,25 @@ class ActorDao(ActorDto):
         return json.loads(df.to_json(orient='records'))
 
     @classmethod
-    def delete_actor_by_setting_state_to_one(cls,id):
-        # This does not delete Actor from the database but rather simply updates 
-        # actor column "state" to 0 which will hide its display from the user 
-        # where as user will think that the selected actor has been deleted
-        session.query(ActorDto).filter(ActorDto.actor_id == id).update({ActorDto.state:"0"}, synchronize_session=False)
-        session.commit()
-        session.close()
-    
-    @classmethod
-    def add_actor_by_setting_state_to_one(cls,id):
-        session.query(ActorDto).filter(ActorDto.actor_id == id).update({ActorDto.state:"1"}, synchronize_session=False)
+    def delete_actor_by_setting_state_to_one(cls, id):
+        # This does not delete the Actor from the database but rather
+        # simply updates actor column "state" to 0 which will hide its
+        # display from the user where as user will think that the
+        # selected actor has been deleted
+        session.query(ActorDto).filter(ActorDto.actor_id == id).update({ActorDto.state: "0"}, synchronize_session=False)
         session.commit()
         session.close()
 
+    @classmethod
+    def add_actor_by_setting_state_to_one(cls, id):
+        session.query(ActorDto).filter(ActorDto.actor_id == id).update({ActorDto.state: "1"}, synchronize_session=False)
+        session.commit()
+        session.close()
 
 
 if __name__ == "__main__":
     ActorDao.bulk()
 
-
-
 # ==============================================================
 # ==============================================================
 # ==============================================================
@@ -236,11 +197,12 @@ if __name__ == "__main__":
 # ==============================================================
 
 
-parser = reqparse.RequestParser()  # only allow price changes, no name changes allowed
+parser = reqparse.RequestParser()
 parser.add_argument('actor_id', type=str, required=True,
-                                        help='This field should be a actorId')
+                    help='This field should be a actorId')
 parser.add_argument('password', type=str, required=True,
-                                        help='This field should be a password')
+                    help='This field should be a password')
+
 
 class Actor(Resource):
     @staticmethod
@@ -255,7 +217,7 @@ class Actor(Resource):
         params_str = ''
         for key in params.keys():
             params_str += 'key: {}, value: {}<br>'.format(key, params[key])
-        return {'code':0, 'message': 'SUCCESS'}, 200
+        return {'code': 0, 'message': 'SUCCESS'}, 200
 
     @staticmethod
     def get(id: str):
@@ -263,39 +225,36 @@ class Actor(Resource):
         try:
             actor = ActorDao.find_by_id(id)
             data = actor.json()
-            
             return data, 200
         except Exception as e:
+            print(e)
             return {'message': 'Actor not found'}, 404
 
     @staticmethod
     def update():
         args = parser.parse_args()
         print(f'Actor {args["id"]} updated ')
-        return {'code':0, 'message': 'SUCCESS'}, 200
+        return {'code': 0, 'message': 'SUCCESS'}, 200
 
     @staticmethod
     def delete(id):
-        print("-------------------------------")
-        print("-------------------------------")
-        print("-------------------------------")
-        print("-------------------------------")
-        ActorDao.delete_actor_by_setting_state_to_one(id)
-        print(f'Actor {id} deleted')
-        return {'code' : 0, 'message' : 'SUCCESS'}, 200    
+        try:
+            ActorDao.delete_actor_by_setting_state_to_one(id)
+            print(f'Actor {id} deleted')
+            return {'code': 0, 'message': 'SUCCESS'}, 200
+        except Exception as e:
+            return e, 404
+
 
 class Actors(Resource):
     @staticmethod
     def post():
         ud = ActorDao()
         ud.bulk('actors')
+
     @staticmethod
     def get():
-        # find all 하지 말고
-        # state 가 1 인 것만!
-        # data = ActorDao.find_all()
         actors = ActorDao.find_state_one()
-        # 여기서 이제 review를 제이슨화 시킨후 보내주면 됨
         data = []
         for actor in actors:
             data.append(actor.json())
@@ -314,6 +273,7 @@ class Access(Resource):
         data = ActorDao.login(actor)
         return data[0], 200
 
+
 class Auth(Resource):
     @staticmethod
     def post():
@@ -321,7 +281,8 @@ class Auth(Resource):
         actor = ActorDto(**body)
         ActorDao.save(actor)
         id = actor.actor_id
-        return {'id': str(id)}, 200 
+        return {'id': str(id)}, 200
+
 
 class AddActor(Resource):
     @staticmethod
@@ -330,9 +291,11 @@ class AddActor(Resource):
             print(name)
             id = ActorDao.find_id_by_name(name)
         except Exception as e:
+            print(e)
             return {'message': 'Actor not found in the database'}, 401
         try:
             ActorDao.add_actor_by_setting_state_to_one(id)
             print(f'Actor {name} added')
         except Exception as e:
-            return {'message': 'Actor Already displayed'}, 404   
+            print(e)
+            return {'message': 'Actor Already displayed'}, 404
--- com_dayoung_api/resources/crawling.py
+++ com_dayoung_api/resources/crawling.py
@@ -1,9 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
-import re # 정규식 사용
-import csv
+import re  # 정규식 사용
 from pandas import DataFrame
-import random
+
 
 class Crawling:
     '''
@@ -11,20 +10,24 @@ class Crawling:
         attributes: ['사진', '나이','이름','본명','종교','소속사', '배우자', '자녀','데뷔년도']
         returns Dataframe with above attributes
         '''
-    def __init__(self, actors_name = ['이병헌'], gender = 'm'):
+    def __init__(self, actors_name=['이병헌'], gender='m'):
         self.actors_name = [(actors_name, gender)]
         self.gender = 'm'
 
     def crawl(self):
         # columns=['사진', '나이','이름','본명','종교','소속사', '배우자', '자녀','데뷔년도']
         # url = "https://ko.wikipedia.org/wiki/"
-        headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"}
-        
+        # headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"}
+
         actors_name = self.actors_name
-        actors_name =[("이병헌","m"),("전지현","f"),("손예진","f"), ("안소희","f"),("강동원","m"),("하정우","m"),
-                    ("김혜수","f"),("현빈","m") ,("유해진","m"),("송강호","m") ,("지창욱","m"), ("이민정","f") 
-                    , ("한효주","f"), ("박보영","f"), ("정해인","m")]
-        
+        actors_name = [("이병헌", "m"), ("전지현", "f"), ("손예진", "f"),
+                       ("안소희", "f"), ("강동원", "m"), ("하정우",  "m"),
+                       ("김혜수",  "f"), ("현빈",  "m"), ("송강호",  "m"),
+                       ("지창욱",  "m"), ("한효주",  "f"), ("정해인",  "m")]
+        # dont add these set
+        # redundant = set(["이병헌", "전지현", "손예진", "안소희", "강동원", "하정우",
+        #                  "김혜수", "현빈", "송강호", "지창욱",  "한효주",  "정해인"])
+
         # actors_url = "https://namu.wiki/w/%EB%B0%B0%EC%9A%B0/%ED%95%9C%EA%B5%AD"
         actors_name_2 = self.crawl_actors_name()
         actors_name.extend(actors_name_2)
@@ -35,18 +38,18 @@ class Crawling:
         return data
 
     def crawl_actors_name(self):
-        headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"}
+        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"}
         # url = self.url
         url = "https://namu.wiki/w/%EB%B0%B0%EC%9A%B0/%ED%95%9C%EA%B5%AD"
         res = requests.get(url, headers=headers)
-        res.raise_for_status() # 혹시 문제가 있을시 에러)
+        res.raise_for_status()  # 혹시 문제가 있을시 에러)
         soup = BeautifulSoup(res.text, 'lxml')
         # 여기 까진 크롤링 하기 전 기본
-        #---------------------------------------------------------------------
+        # ---------------------------------------------------------------------
         # 여기부터 크롤링 시작
-        headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"}
+        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"}
         res = requests.get(url, headers=headers)
-        res.raise_for_status() # 혹시 문제가 있을시 에러
+        res.raise_for_status()  # 혹시 문제가 있을시 에러
         soup = BeautifulSoup(res.text, 'lxml')
         list_div = soup.find_all('div', {'class': 'wiki-heading-content'})
         elements = ""
@@ -60,44 +63,49 @@ class Crawling:
                     actors2.append(actor.text)
         return actors2
 
-    
     def actors_to_df(self, actors_name, actor_id):
-        headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"}
-        
+        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"}
         actors = []
-        for name in actors_name :
+        redundant = set(["이병헌", "전지현", "손예진", "안소희", "강동원", "하정우",
+                         "김혜수", "현빈", "송강호", "지창욱",  "한효주",  "정해인"])
+        for name in actors_name:
             actor_info = {}
             if type(name) == tuple:
                 # 첫 화면에 보여줄 배우들
-                self.gender = name[1] # gender
-                name = name[0] # name
-            if name == "갈소원": # 여기 부터 여성 배우들 출력
+                self.gender = name[1]  # gender
+                name = name[0]  # name
+            else:
+                if name in redundant:  # 처음 넣은 배우들은 중복으로 들어가지 않게 스킵
+                    continue
+            if name == "갈소원":  # 여기 부터 여성 배우들 출력
                 self.gender = 'f'
             url = "https://ko.wikipedia.org/wiki/"
             url += name
             res = requests.get(url, headers=headers)
-            try: 
+            try:
                 res.raise_for_status()
-            except:
+            except Exception as e:
+                print(e)
                 continue
             soup = BeautifulSoup(res.text, 'lxml')
             # --------------------------------------------- 위키 드감
-            table = soup.find('table', attrs = {"class":"infobox"})
-            
+            table = soup.find('table', attrs={"class": "infobox"})
+
             # if table.has_next
             if not table:
-                # print(name, "이름의 유명인이 많음으로 인해 제외 합니다")
+                # 같은 이름의 유명인은 스킵
                 continue
             tables = table.find_all("tr")
-            url_table = tables[1].find('a',attrs={"class":"image"})
+            url_table = tables[1].find('a', attrs={"class": "image"})
             if not url_table:
-                # print("문제 있음")
+                # 이미지 파일 없는 연예인 스킵
                 continue
             url2 = url_table.find('img')['src']
             if len(url2) > 200:
+                # 너무 긴 url 은 많은 데이터를 차지 해서 스킵
                 continue
             actor_info['photo_url'] = url2
-            
+
             tables = tables[2:]
             actor = {}
             for table in tables:
@@ -107,11 +115,7 @@ class Crawling:
                         continue
                     td = table.td.get_text()
                     actor[th] = td
-            # 데이터 정리
-            # 출생 -> 나이
-            # print(name)
-            # print(actor)
-            p = re.compile("..세") # 30세 56세 등등
+            p = re.compile("..세")  # 30세 56세 등등
             if '출생' not in actor.keys():
                 continue
             if not p.search(actor['출생']):
@@ -120,8 +124,6 @@ class Crawling:
             age = age[:-1]
             actor_info['age'] = age
             actor_info['actor_id'] = actor_id
-            
-            
             # 가명 없을 시 없다고 표시 본명에 가명 없음 이라고 표시
             actor_info['name'] = name
             if '본명' not in actor.keys():
@@ -138,6 +140,7 @@ class Crawling:
                 actor_info['agency'] = 'no angency'
             else:
                 if len(actor['소속사']) > 30:
+                    # 너무 긴 소속사 이름 패스
                     continue
                 actor_info['agency'] = actor['소속사']
             # 배우자
@@ -145,20 +148,20 @@ class Crawling:
                 actor_info['spouse'] = 'no spouse'
             else:
                 if len(actor['배우자']) > 30:
+                    # 긴 배우자 이름 패스
                     continue
                 actor_info['spouse'] = actor['배우자']
-            # 자녀
 
             if '자녀' not in actor.keys():
                 actor_info['children'] = 'no child'
             else:
-                if len(actor['자녀'])>100:
+                if len(actor['자녀']) > 100:
+                    # 긴 자녀 이름 패스
                     continue
                 actor_info['children'] = actor['자녀']
-                
-            
-            # 활동 기간 - 정규식 이용
-            # 데뷔년도
+
+            #  활동 기간 - 정규식 이용
+            #  데뷔년도
             p = re.compile('....년')
             if '활동 기간' not in actor.keys():
                 continue
@@ -170,24 +173,33 @@ class Crawling:
             actors.append(actor_info)
             actor_id +=1
             if actor_id < 13:
+                # 처음 내가 보여주고 싶은 12명
                 actor_info['state'] = "1"
             else:
+                # 나머지는 블라인드 처리
                 actor_info['state'] = "0"
-            if actor_id%50 == 10:
+            if actor_id % 50 == 10:
                 print("working on it", actor_id)
                 print("하고 있어!!!!!!!")
                 print("하고 있어!!!!!!!")
                 print("하고 있어!!!!!!!")
                 print("하고 있어!!!!!!!")
                 print("하고 있어!!!!!!!")
+                # 지금 crawling 되고 있는지 확인
         print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
 
-        # dict_keys(['photo_url', 'age', 'actor_id', 'name', 'real_name', 'religion', 'agency', 'spouse', 'children', 'debut_year', 'gender'])
-        data = DataFrame(actors, columns=['photo_url', 'age', 'actor_id', 'name', 'real_name', 'religion', 'agency', 'spouse', 'children', 'debut_year', 'gender','state'])
+        # dict_keys(['photo_url', 'age', 'actor_id', 'name', 'real_name',
+        #            'religion', 'agency', 'spouse', 'children', 'debut_year',
+        #            'gender'])
+        # List of Dict 을 데이터 프레임으로 만들어주기
+        data = DataFrame(actors,
+                         columns=['photo_url', 'age', 'actor_id', 'name',
+                                  'real_name', 'religion', 'agency', 'spouse',
+                                  'children', 'debut_year', 'gender', 'state'])
         return data
 
 
-
+# 이 코딩만 확인 하고 싶을 시
 # if __name__ == '__main__':
 #     c = Crawling()
 #     c.crawl()
